209f65b524106cf55f6d1cde4a887e48f27e3486
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2025-01-24 08:04:16 -0800

    update comment

--- scipy/ndimage/tests/test_morphology.py
+++ scipy/ndimage/tests/test_morphology.py
@@ -2510,7 +2510,7 @@ class TestNdimageMorphology:
         assert_array_almost_equal(output, expected)
 
     @skip_xp_backends("jax.numpy", reason="output array is read-only.")
-    @skip_xp_backends("dask.array", reason="output kw doesn't make sense for dask")
+    @skip_xp_backends("dask.array", reason="converts dask output array to numpy")
     def test_white_tophat01(self, xp):
         array = xp.asarray([[3, 2, 5, 1, 4],
                             [7, 6, 9, 3, 5],
@@ -2564,7 +2564,7 @@ class TestNdimageMorphology:
         xp_assert_equal(output, expected)
 
     @skip_xp_backends("jax.numpy", reason="output array is read-only.")
-    @skip_xp_backends("dask.array", reason="output kw doesn't make sense for dask")
+    @skip_xp_backends("dask.array", reason="converts dask output array to numpy")
     def test_white_tophat04(self, xp):
         array = np.eye(5, dtype=bool)
         structure = np.ones((3, 3), dtype=bool)
@@ -2577,7 +2577,7 @@ class TestNdimageMorphology:
         ndimage.white_tophat(array, structure=structure, output=output)
 
     @skip_xp_backends("jax.numpy", reason="output array is read-only.")
-    @skip_xp_backends("dask.array", reason="output kw doesn't make sense for dask")
+    @skip_xp_backends("dask.array", reason="converts dask output array to numpy")
     def test_black_tophat01(self, xp):
         array = xp.asarray([[3, 2, 5, 1, 4],
                             [7, 6, 9, 3, 5],
@@ -2632,7 +2632,7 @@ class TestNdimageMorphology:
         xp_assert_equal(output, expected)
 
     @skip_xp_backends("jax.numpy", reason="output array is read-only.")
-    @skip_xp_backends("dask.array", reason="output kw doesn't make sense for dask")
+    @skip_xp_backends("dask.array", reason="converts dask output array to numpy")
     def test_black_tophat04(self, xp):
         array = xp.asarray(np.eye(5, dtype=bool))
         structure = xp.asarray(np.ones((3, 3), dtype=bool))
