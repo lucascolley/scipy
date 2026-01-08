e5b1f23bd1199fa9536f3df1fd296e2ec844aba0
Lucas Colley <lucas.colley8@gmail.com>
2026-01-08 14:27:37 +0000

    LICENSE.BSD, `tools/vendoring`, READMEs

--- tools/vendoring/vendor_packaging_version.sh
+++ tools/vendoring/vendor_packaging_version.sh
@@ -8,7 +8,7 @@ set -o errexit
 REPO_URL="https://github.com/pypa/packaging"
 COMMIT_HASH="f58537628042c7f29780b9d33f31597e7fc9d664"
 
-# XXX: run this from the repo top level like `./tools/vendor_packaging_version.sh`
+# XXX: run this from the repo top level like `./tools/vendoring/vendor_packaging_version.sh`
 ROOT_DIR="scipy/_external/packaging_version/src"
 
 rm -rf $ROOT_DIR
@@ -20,4 +20,5 @@ git checkout $COMMIT_HASH
 popd
 mv -v $ROOT_DIR/.tmp/src/packaging/version.py $ROOT_DIR/
 mv -v $ROOT_DIR/.tmp/src/packaging/_structures.py $ROOT_DIR/
+mv -v $ROOT_DIR/.tmp/LICENSE.BSD $ROOT_DIR/
 rm -rf $ROOT_DIR/.tmp
